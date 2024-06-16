6edb814f2e20705fd6ed8b07c75129feb95f0508
Ravskhan <ravshansam52@gmail.com>
2024-06-16 17:09:55 +0500

    salom asilbek

--- apps/base/urls.py
+++ apps/base/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path, include
 
-from .views import home_page, faoliyat
+from .views import home_page, faoliyat, contactus
 
 
 urlpatterns = [
     path("", home_page, name="home_page"),
-    path("gallery/", include("apps.gallery.urls")),
-    path("library/", include("apps.library.urls")),
-    path("news/", include("apps.news.urls")),
     path("faoliyat/", faoliyat, name="faoliyat"),
+    path("contactus/", contactus, name="contactus"),
 ]
 
 
