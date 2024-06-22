f48fa24955fc0e4b6d4ad9998b93e1db7568f5bf
Ravskhan <ravshansam52@gmail.com>
2024-06-22 12:20:22 +0500

    22 kuni qilindi

--- apps/news/models.py
+++ apps/news/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from ckeditor.fields import RichTextField
+from django.utils.html import strip_tags
 
 # Create your models here.
 
@@ -19,13 +20,24 @@ class News(models.Model):
     title = models.CharField(max_length=1023)
     description = RichTextField()
     photo = models.ImageField(upload_to='news/', default='back1.jpg')
-
+    min_desc = models.TextField(max_length=200, blank=True)  # yangi o'zgaruvchi
     views = models.IntegerField(default=1)
     added_date = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ['-added_date']
 
+
+    def save(self, *args, **kwargs):
+        # HTML teglarini olib tashlash
+        plain_text = strip_tags(self.description)
+        # Matnni 200 ta belgi bilan cheklash
+        max_length = 100
+        if len(plain_text) > max_length:
+            self.min_desc = plain_text[:max_length]
+        else:
+            self.min_desc = plain_text
+        super(News, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.title[:50]
